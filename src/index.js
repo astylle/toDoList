730f63930f0d652e1b9493c329ffd53d6f7fb93c
user_hc <100005741+astylle@users.noreply.github.com>
2025-02-03 10:04:11 +0000

    local storage implemented

--- src/index.js
+++ src/index.js
@@ -1,73 +1,40 @@
 import './style.css';
-import {getList} from './taskFeatures/taskCategory';
+import {displayTask} from "./taskFeatures/existingTasks";
+import { formCollectInfo, displayExistingTasks,displayAllTasks} from './taskFeatures/formFeatures';
+
+const defaultList = "All Tasks";
+let currentList = defaultList;
+let title = document.getElementById("title");
+
 
 let form = document.getElementById("taskForm");
-let taskList = document.getElementById("taskList");
+form.addEventListener("submit", formCollectInfo);
+displayExistingTasks();
 
 
-//task object
-function task(taskCat, taskName, taskDesc, taskDate, taskPrio){
-    this.taskCat = taskCat;
-    this.taskName = taskName;
-    this.taskDesc = taskDesc;
-    this.taskDate = taskDate;
-    this.taskPrio = taskPrio;
-}
-
-
-
-//takes info from the form
-form.addEventListener("submit", function(e) {
-    e.preventDefault();
-
-    const taskCat = getList();
-    const taskName = document.getElementById("taskName").value;
-    const taskDesc = document.getElementById("taskDescription").value;
-    const taskDate = document.getElementById("taskDate").value;
-    const taskPrio = document.getElementById("priority").value
-    // const task
-
-    createTask(taskCat, taskName, taskDesc, taskDate, taskPrio);
-    
-    form.reset();
+let buttonsAllTasks = document.querySelector(".allTasks");
+buttonsAllTasks.addEventListener("click", (e)=>{
+    title.innerHTML="All Tasks"
+    displayAllTasks(currentList);
 });
 
-//task 
-function createTask(taskCat, name, desc, date, prio){
-    const displayTask = new task(taskCat, name, desc, date, prio)
+let buttonsProject = document.querySelectorAll(".projects");
 
-    createTaskStructure(taskCat, name, desc,date, prio)
 
-    console.log("Create Task Cat: " + taskCat)
-    console.log("Create Task Name: " + name);
-    console.log("Create Task Description: " + desc);
-    console.log("Create Task Date: " + date);
-    console.log("Create Task Prio: " + prio);
-}
-//can probably turn this into function factory
-function createTaskStructure(cat, name, desc, date, prio){
-    const task = document.createElement("tr");
-    
-    const taskCat = document.createElement("td");
-    taskCat.innerText = cat;
-    
-    const taskName = document.createElement("td");
-    taskName.innerText = name;
+buttonsProject.forEach(buttonsProject => {
+    buttonsProject.addEventListener("click", (e) => { 
+        currentList = e.target.textContent;        
+        title.innerHTML= currentList;
+        displayExistingTasks(currentList);
+        
+    });
+}); 
 
-    const taskDesc = document.createElement("td");
-    taskDesc.innerText = desc;
 
-    const taskDate = document.createElement("td");
-    taskDate.innerText = date;
 
-    const taskPrio = document.createElement("td");
-    taskPrio.innerText = prio;
 
-    task.appendChild(taskCat);
-    task.appendChild(taskName);
-    task.appendChild(taskDesc);
-    task.appendChild(taskDate);
-    task.appendChild(taskPrio);
+export function getList(){
+    return currentList;
+};
 
-    taskList.appendChild(task);
-}
+export default form;
---
+++ src/taskFeatures/formFeatures.js
@@ -0,0 +1,115 @@
+import form, { getList } from "../index";
+
+let taskList = document.getElementById("taskList");
+const key = "taskList";
+let storedArray = JSON.parse(localStorage.getItem(key)) || [];
+// let taskLength = JSON.parse(localStorage.getItem(key).length);
+// console.log("the current task length is " + taskLength);
+
+let clearButton = document.querySelector(".clearTasks");
+clearButton.addEventListener("click", ()=>{
+    localStorage.clear()
+    storedArray = [];
+    taskList.innerHTML="";
+})
+
+//task object
+function Task(taskCat, taskName, taskDesc, taskDate, taskPrio){
+    this.taskCat = taskCat;
+    this.taskName = taskName;
+    this.taskDesc = taskDesc;
+    this.taskDate = taskDate;
+    this.taskPrio = taskPrio;
+}
+
+export function formCollectInfo(e){
+    e.preventDefault();
+
+    //collects form data
+    const taskCat = getList();
+    const taskName = document.getElementById("taskName").value;
+    const taskDesc = document.getElementById("taskDescription").value;
+    const taskDate = document.getElementById("taskDate").value;
+    const taskPrio = document.getElementById("priority").value
+    
+    // Creates and saves task to localStorage
+    let task = new Task(taskCat, taskName, taskDesc, taskDate, taskPrio);
+    storedArray.push(task)
+    console.log(storedArray);
+
+    localStorage.setItem(key, JSON.stringify(storedArray));
+
+    console.log(localStorage);
+    console.log("Form reset and task saved to localStorage");
+
+    createTask(task)
+    form.reset()
+};
+
+//takes info from the form
+// form.addEventListener("submit", formCollectInfo(e))
+
+// //creates task
+// createTask();
+
+//can probably turn this into function factory
+
+export function displayAllTasks(){
+    taskList.innerHTML="";
+    storedArray.forEach(task => {
+        console.log("task name is: " + task.taskName);
+        createTask(task);
+    })
+}
+
+export function displayExistingTasks (currentList){
+    // let taskLength = JSON.parse(localStorage.getItem(key)).length;
+    // console.log("the current task length is " + taskLength);
+    taskList.innerHTML="";
+    storedArray.forEach(task => {
+        if(task.taskCat==currentList){
+            console.log("task category is: " + task.taskCat);
+            createTask(task);
+        }
+    });
+}
+
+
+
+ function createTask(task){
+    const taskObject = task;
+
+    const displayTask = document.createElement("tr");
+
+    const taskCat = document.createElement("td");
+    taskCat.innerText = taskObject.taskCat;
+    
+    const taskName = document.createElement("td");
+    taskName.innerText = taskObject.taskName;
+
+    const taskDesc = document.createElement("td");
+    taskDesc.innerText = taskObject.taskDesc;
+
+    const taskDate = document.createElement("td");
+    taskDate.innerText = taskObject.taskDate;
+
+    const taskPrio = document.createElement("td");
+    taskPrio.innerText = taskObject.taskPrio;
+
+    // console.log("Create Task Cat: " + taskObject.taskCat)
+    // console.log("Create Task Name: " + taskObject.taskName);
+    // console.log("Create Task Description: " + taskObject.taskDesc);
+    // console.log("Create Task Date: " + taskObject.taskDate);
+    // console.log("Create Task Prio: " + taskObject.taskPrio);
+
+    displayTask.appendChild(taskCat);
+    displayTask.appendChild(taskName);
+    displayTask.appendChild(taskDesc);
+    displayTask.appendChild(taskDate);
+    displayTask.appendChild(taskPrio);
+
+    taskList.appendChild(displayTask);
+
+}
+
+ 
